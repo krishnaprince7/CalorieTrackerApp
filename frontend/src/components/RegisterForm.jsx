7dd55e819266258f7e551b6904b72bf8a78de1ca
krishna vishwakama <146770906+krishnaprince7@users.noreply.github.com>
2024-05-31 11:14:30 +0530

    Update RegisterForm.jsx

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Navebar from './Navebar';
+import { Link, useNavigate } from 'react-router-dom';
 import Footer from './Footer';
-import { Link } from 'react-router-dom';
+import Navebar from './Navebar'; 
 
 const RegisterForm = () => {
     const [formData, setFormData] = useState({
@@ -12,8 +12,31 @@ const RegisterForm = () => {
         password: ''
     });
 
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false); 
     const [successMessage, setSuccessMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true); 
+        setError(''); 
+        setSuccessMessage('');
+
+        setTimeout(async () => { 
+            try {
+                const response = await axios.post('http://localhost:3001/register', formData);
+                console.log('Response:', response.data); 
+                setLoading(false); 
+                setSuccessMessage('Account created successfully. Redirecting to login...');
+                navigate('/login'); // Redirect to login after registration
+            } catch (error) {
+                console.error('Registration Error:', error.response || error); 
+                setError(error.response?.data?.message || 'Error registering. Please try again.');
+                setLoading(false); 
+            }
+        }, 2000); // 2 seconds delay
+    };
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -23,73 +46,85 @@ const RegisterForm = () => {
         });
     };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        // Basic client-side validation
-        if (!formData.name || !formData.email || !formData.age || !formData.password) {
-            setErrorMessage('All fields are required.');
-            setSuccessMessage('');
-            return;
-        }
-
-        try {
-            const response = await axios.post('https://calorie-tracker-app-orcin.vercel.app/register', formData);
-            console.log('Employee registered:', response.data);
-            setSuccessMessage('Account created successfully go to Login.');
-            setErrorMessage('');
-            // Reset form after successful registration if needed
-            setFormData({
-                name: '',
-                email: '',
-                age: '',
-                password: ''
-            });
-        } catch (error) {
-            console.error('Error registering employee:', error.response.data); // Log the actual error response
-            setErrorMessage(error.response?.data?.message || 'Error creating account. Please try again.');
-            setSuccessMessage('');
-        }
-    };
-
     return (
         <>
-            <Navebar />
+            <Navebar /> 
             <div className="max-w-lg mx-auto my-10 px-6 py-8 bg-white shadow-2xl rounded-lg border border-gray-200">
                 <h2 className="text-3xl mb-6 text-center font-semibold text-gray-700">Register</h2>
-                {successMessage && (
-                    <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-                        <strong className="font-bold">Success!</strong>
-                        <span className="block sm:inline"> {successMessage}</span>
-                    </div>
-                )}
-                {errorMessage && (
+                {error && (
                     <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
                         <strong className="font-bold">Error!</strong>
-                        <span className="block sm:inline"> {errorMessage}</span>
+                        <span className="block sm:inline"> {error}</span>
                     </div>
                 )}
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="name">Name</label>
-                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
+                {loading ? (
+                    <div className="text-center my-4">
+                        <div className="loader border-t-4 border-b-4 border-blue-500 w-12 h-12 rounded-full mx-auto animate-spin"></div>
                     </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">Email</label>
-                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="age">Age</label>
-                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" type="number" name="age" value={formData.age} onChange={handleChange} placeholder="Age" />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700 font-bold mb-2" htmlFor="password">Password</label>
-                        <input className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" />
-                    </div>
-                    <button className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-300 font-semibold">Register</button>
-                    <div className="mt-4 text-center">
-                    <Link to="/login" className="text-blue-500 hover:underline"> Already Have an account? Login</Link>
+                ) : (
+                    <>
+                        {successMessage && (
+                            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
+                                <strong className="font-bold">Success!</strong>
+                                <span className="block sm:inline"> {successMessage}</span>
+                            </div>
+                        )}
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2" htmlFor="name">Name</label>
+                                <input 
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" 
+                                    type="text" 
+                                    value={formData.name} 
+                                    onChange={handleChange} 
+                                    name="name" 
+                                    placeholder="Name" 
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2" htmlFor="email">Email</label>
+                                <input 
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" 
+                                    type="email" 
+                                    value={formData.email} 
+                                    onChange={handleChange} 
+                                    name="email" 
+                                    placeholder="Email" 
+                                    required
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-gray-700 font-bold mb-2" htmlFor="age">Age</label>
+                                <input 
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" 
+                                    type="number" 
+                                    value={formData.age} 
+                                    onChange={handleChange} 
+                                    name="age" 
+                                    placeholder="Age" 
+                                    required
+                                />
+                            </div>
+                            <div className="mb-6">
+                                <label className="block text-gray-700 font-bold mb-2" htmlFor="password">Password</label>
+                                <input 
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 transition duration-300" 
+                                    type="password" 
+                                    value={formData.password} 
+                                    onChange={handleChange} 
+                                    name="password" 
+                                    placeholder="Password" 
+                                    required
+                                />
+                            </div>
+                            <button className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition duration-300 font-semibold" type="submit">Register</button>
+                        </form>
+                    </>
+                )}
+                <div className="mt-4 text-center">
+                    <Link to="/login" className="text-blue-500 hover:underline">Already have an account? Login</Link>
                 </div>
-                </form>
             </div>
             <Footer />
         </>
@@ -97,6 +132,3 @@ const RegisterForm = () => {
 };
 
 export default RegisterForm;
-
-
-//https://calorie-tracker-app-orcin.vercel.app/
